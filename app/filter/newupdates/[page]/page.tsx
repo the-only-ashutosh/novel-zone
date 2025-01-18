ed2ac05c9d7933b5ed6b29a7b09b919922a9351e
Ashutosh <ashutosh.nebe@gmail.com>
2025-01-18 19:02:54 +0530

    New updates metadata removed.

--- app/filter/newupdates/[page]/page.tsx
+++ app/filter/newupdates/[page]/page.tsx
@@ -1,6 +1,4 @@
 import React, { Suspense } from "react";
-import { fetchRecentUpdatesPage } from "@/service/dataoperation";
-import { Metadata } from "next";
 import GradBanner from "@/components/Shared/GradBanner";
 import NewUpdatesList from "@/components/Elements/NewUpdates/NewUpdatesList";
 import NewUpdatesSkeleton from "@/components/Elements/NewUpdates/NewUpdatesSkeleton";
@@ -19,40 +17,3 @@ const NewUpdatesPage = ({ params }: { params: Promise<{ page: number }> }) => {
 };
 
 export default NewUpdatesPage;
-
-export async function generateMetadata({
-  params,
-}: {
-  params: Promise<{ page: number }>;
-}): Promise<Metadata> {
-  const { page } = await params;
-  const book = await fetchRecentUpdatesPage(page);
-  const books =
-    book !== "Invalid Page"
-      ? book.data.map((bk) => {
-          return bk.title;
-        })
-      : ["Recent Updates", "New Updates"];
-  const images =
-    book !== "Invalid Page"
-      ? book.data.map((bk) => {
-          return bk.book.imageUrl;
-        })
-      : [];
-  return {
-    title: "Newly Updated Novels",
-    keywords: [
-      "Novel",
-      "Novel Zone",
-      "Novelbin",
-      "lightnovel",
-      "webnovel",
-      ...books,
-    ],
-    twitter: {
-      card: "summary_large_image",
-      title: "Newly Updated Books",
-      images: [...images], // Must be an absolute URL
-    },
-  };
-}
--- app/filter/newupdates/page.tsx
+++ app/filter/newupdates/page.tsx
@@ -1,6 +1,4 @@
 import React, { Suspense } from "react";
-import { fetchRecentUpdatesPage } from "@/service/dataoperation";
-import { Metadata } from "next";
 import GradBanner from "@/components/Shared/GradBanner";
 import NewUpdatesList from "@/components/Elements/NewUpdates/NewUpdatesList";
 import NewUpdatesSkeleton from "@/components/Elements/NewUpdates/NewUpdatesSkeleton";
@@ -20,35 +18,3 @@ const NewUpdates = () => {
 };
 
 export default NewUpdates;
-
-export async function generateMetadata(): Promise<Metadata> {
-  const book = await fetchRecentUpdatesPage();
-  const books =
-    book !== "Invalid Page"
-      ? book.data.map((bk) => {
-          return bk.title;
-        })
-      : ["Recent Updates", "New Updates"];
-  const images =
-    book !== "Invalid Page"
-      ? book.data.map((bk) => {
-          return bk.book.imageUrl;
-        })
-      : [];
-  return {
-    title: "Newly Updated Novels",
-    keywords: [
-      "Novel",
-      "Novel Zone",
-      "Novelbin",
-      "lightnovel",
-      "webnovel",
-      ...books,
-    ],
-    twitter: {
-      card: "summary_large_image",
-      title: "Newly Updated Books",
-      images: [...images], // Must be an absolute URL
-    },
-  };
-}
