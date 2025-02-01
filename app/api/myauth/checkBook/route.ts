4568717d82a2c4464ccda7c6e592da30a1e56c61
Ashutosh <ashutosh.nebe@gmail.com>
2025-02-01 18:16:46 +0530

    Images, Button, Search Modal bug fixed.

--- app/api/myauth/checkBook/route.ts
+++ app/api/myauth/checkBook/route.ts
@@ -1,16 +1,7 @@
-import prisma from "@/service/client";
 import { checkBook } from "@/service/dataoperation";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const book = await req.json();
   return NextResponse.json(await checkBook(book));
-}
-
-export async function GET() {
-  const books = await prisma.book.findMany({
-    select: { bookUrl: true, imageUrl: true },
-    orderBy: { id: "asc" },
-  });
-  return NextResponse.json({ books });
 }
